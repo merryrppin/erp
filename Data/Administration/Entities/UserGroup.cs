d2c47b2c2eec45b83d7188e56cf465df98ed18c0
Wilmar González <merryrppin@hotmail.com>
2019-11-17 20:31:07 -0500

    Entidades de menu y manejo de permisos de menu

---
+++ Data/Administration/Entities/UserGroup.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Data.Administration.Entities
+{
+    [Table("tblUserGroup", Schema = "dbo")]
+    public class UserGroup
+    {
+        [Key]
+        public int UserGroupId { get; set; }
+        [MaxLength(100)]
+        [Required]
+        public string Name { get; set; }
+        [MaxLength(500)]
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public bool Active { get; set; }
+    }
+}
