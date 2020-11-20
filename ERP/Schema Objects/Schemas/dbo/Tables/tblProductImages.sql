470ed6bc4cd7e0611df8010d9082d5434b050973
Alexis Cano <alexis.cano@globostudio.net>
2020-11-20 11:24:15 -0500

    creacion tabla images product

---
+++ ERP/Schema Objects/Schemas/dbo/Tables/tblProductImages.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[tblProductImages]
+(
+	[ProductImageId] INT NOT NULL IDENTITY(1,1) PRIMARY KEY, 
+    [ProductCode] VARCHAR(50) NOT NULL, 
+    [Image] IMAGE NOT NULL
+)
+GO	
+
+ALTER TABLE [dbo].[tblProductImages]  WITH CHECK ADD  CONSTRAINT [FK_tblProductImages_tblProduct_ProductCode] FOREIGN KEY([ProductCode])
+REFERENCES [dbo].[tblProduct] ([ProductCode])
