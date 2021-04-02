baf934024ae702747551187d2d6ae7bb60d83671
Alexis Cano <jalexiscc@hotmail.com>
2021-04-02 13:58:52 -0500

    Se agrega el identity a la clave primaria

--- ERP/Schema Objects/Schemas/dbo/Tables/tblWarehouse.sql
+++ ERP/Schema Objects/Schemas/dbo/Tables/tblWarehouse.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[tblWarehouse]
 (
-	[WarehouseId] INT NOT NULL PRIMARY KEY, 
+	[WarehouseId] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
     [WarehouseCode] VARCHAR(10) NOT NULL, 
     [Description] VARCHAR(100) NOT NULL, 
     [Default] BIT NOT NULL DEFAULT 0, 
